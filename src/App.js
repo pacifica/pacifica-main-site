5d07c318dd03b9b4e897d9e1a918cde5d86af058
David Brown <dmlb2000@gmail.com>
2019-05-07 11:12:49 -0700

    Add under construction site.
    Signed-off-by: David Brown <dmlb2000@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import UnderConstruction from "react-under-construction";
+import "react-under-construction/build/css/index.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UnderConstruction
+      background={{
+        image:
+          "https://static.pexels.com/photos/259698/pexels-photo-259698.jpeg",
+        textColor: "#fff",
+        overlay: {
+          color: "#000",
+          opacity: ".5"
+        }
+      }}
+      logo={{
+        src: "https://image.ibb.co/b7guP5/Rubbby_without_text.png",
+        alt: "alt text"
+      }}
+      title={{
+        text: "Pacifica Software"
+      }}
+      description={{
+        text:
+          "Our website is under construction. We'll be here soon, follow us on Twitter to keep notified.",
+        style: {
+          maxWidth: "440px"
+        }
+      }}
+      links={[
+        {
+          url: "https://www.twitter.com/PacificaSoftwa1",
+          image: "https://image.flaticon.com/icons/svg/145/145812.svg"
+        },
+        {
+          url: "mailto:dmlb2000@gmail.com",
+          image: "https://image.flaticon.com/icons/svg/321/321817.svg"
+        }
+      ]}
+    />
   );
 }
 
