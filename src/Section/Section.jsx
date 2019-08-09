5734d1da91385e054f4f7abc1ff652b568169935
David Brown <dmlb2000@gmail.com>
2019-08-09 16:48:00 -0700

    Fix eslint
    Signed-off-by: David Brown <dmlb2000@gmail.com>

--- src/Section/Section.jsx
+++ src/Section/Section.jsx
@@ -5,7 +5,10 @@ const Section = function Section (data, level) {
   return (
     <div key={`block-${level}`}>
       <hr className="home-divider" />
-      {React.createElement(`h${level}`, { key: `header-${level}`, className: 'home-title' }, data.title)}
+      {React.createElement(`h${level}`, {
+        className: 'home-title',
+        key: `header-${level}`
+      }, data.title)}
       {SectionContent(data.content, level)}
     </div>
   )
