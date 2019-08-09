ff4b37e114ab77a713c80abc965f5533dc272ead
David Brown <dmlb2000@gmail.com>
2019-08-09 09:27:59 -0700

    This stubs out docs page like about.
    This stubs out the docs page like the about page.
    
    Signed-off-by: David Brown <dmlb2000@gmail.com>

--- src/Docs/Docs.jsx
+++ src/Docs/Docs.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import Section from '../Section'
+import getText from '../Utils'
 
 const Docs = function Docs () {
   return (
     <div>
-      <p>
-        {'Docs about Pacifica'}
-      </p>
+      {
+        // We are starting with h2 that's why 2
+        // eslint-disable-next-line no-magic-numbers
+        Section(getText().docspage, 2)
+      }
     </div>
   )
 }
