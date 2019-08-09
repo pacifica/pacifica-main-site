4862a7a99aa0bb52b0f1e0e58793b10baba8cd22
David Brown <dmlb2000@gmail.com>
2019-08-09 16:15:44 -0700

    Add Dark Grey Logo
    This adds a dark grey logo that matches the title color #3f3f3f
    
    Fix #18
    
    Signed-off-by: David Brown <dmlb2000@gmail.com>

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,6 +1,7 @@
+import { LinkContainer } from 'react-router-bootstrap'
 import React from 'react'
 import getText from '../Utils'
-import logo from '../assets/Pacifica-64x64-black.png'
+import logo from '../assets/Pacifica-64x64-darkgrey.png'
 // I'm not sure why this isn't sorting properly
 // eslint-disable-next-line sort-imports
 import NavDropdown from './NavDropdown.jsx'
@@ -11,11 +12,13 @@ const Header = function Header () {
     <div className="header">
       <div className="logo-block">
         <div className="logo-img-block">
-          <img
-            alt={title}
-            className="logo"
-            src={logo}
-          />
+          <LinkContainer to="/">
+            <img
+              alt={title}
+              className="logo"
+              src={logo}
+            />
+          </LinkContainer>
         </div>
         <div className="logo-title-block">
           <h1>
