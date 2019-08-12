e56904c6eee8d85e27384d26f3a53cdcebb2cd37
David Brown <dmlb2000@gmail.com>
2019-08-12 07:36:33 -0700

    All the fixes... (#26)
    * Remove Helmet
    
    We don't need to require helmet as we control index.html.
    
    Signed-off-by: David Brown <dmlb2000@gmail.com>
    
    * Add Dark Grey Logo
    
    This adds a dark grey logo that matches the title color #3f3f3f
    
    Fix #18
    
    Signed-off-by: David Brown <dmlb2000@gmail.com>
    
    * Add theming to About page
    
    Fix #19 apply styles to About and Docs page.
    
    Signed-off-by: David Brown <dmlb2000@gmail.com>
    
    * Fix eslint
    
    Signed-off-by: David Brown <dmlb2000@gmail.com>

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,7 +1,7 @@
-import { Helmet } from 'react-helmet'
+import { LinkContainer } from 'react-router-bootstrap'
 import React from 'react'
 import getText from '../Utils'
-import logo from '../assets/Pacifica-64x64-black.png'
+import logo from '../assets/Pacifica-64x64-darkgrey.png'
 // I'm not sure why this isn't sorting properly
 // eslint-disable-next-line sort-imports
 import NavDropdown from './NavDropdown.jsx'
@@ -10,21 +10,15 @@ const Header = function Header () {
   const { title } = getText().header
   return (
     <div className="header">
-      <Helmet>
-        <link
-          crossOrigin="anonymous"
-          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-          rel="stylesheet"
-        />
-      </Helmet>
       <div className="logo-block">
         <div className="logo-img-block">
-          <img
-            alt={title}
-            className="logo"
-            src={logo}
-          />
+          <LinkContainer to="/">
+            <img
+              alt={title}
+              className="logo"
+              src={logo}
+            />
+          </LinkContainer>
         </div>
         <div className="logo-title-block">
           <h1>
--- src/Section/Section.jsx
+++ src/Section/Section.jsx
@@ -4,7 +4,11 @@ import SectionContent from './SectionContent.jsx'
 const Section = function Section (data, level) {
   return (
     <div key={`block-${level}`}>
-      {React.createElement(`h${level}`, { key: `header-${level}` }, data.title)}
+      <hr className="home-divider" />
+      {React.createElement(`h${level}`, {
+        className: 'home-title',
+        key: `header-${level}`
+      }, data.title)}
       {SectionContent(data.content, level)}
     </div>
   )
