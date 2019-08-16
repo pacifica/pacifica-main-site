8c25c05005fc9e6c79de24f32897e85fb0e5340c
David Brown <dmlb2000@gmail.com>
2019-08-16 10:23:52 -0700

    Add Mission and Vision page.
    Signed-off-by: David Brown <dmlb2000@gmail.com>

--- src/Mission/Mission.jsx
+++ src/Mission/Mission.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import getText from '../Utils'
 
 const Mission = function Mission () {
+  const { mission, vision } = getText()
   return (
     <div>
-      {`Some Mission Statement`}
+      <div className="home-text-block">
+        <h2>
+          {`Mission`}
+        </h2>
+      </div>
+      <div className="home-text-block">
+        <p className="home-text">
+          {mission}
+        </p>
+      </div>
+      <hr className="home-divider" />
+      <div className="home-text-block">
+        <h3>
+          {`Vision`}
+        </h3>
+      </div>
+      <div className="home-text-block">
+        <p className="home-text">
+          {vision}
+        </p>
+      </div>
     </div>
   )
 }
