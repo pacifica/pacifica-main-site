28cdc562ed006573c5e3164887c079dc6f4ffc1a
David Brown <dmlb2000@gmail.com>
2019-08-26 13:39:00 -0700

    Add Pages for Python and Docker
    This adds the content for Docker and Python
    
    Signed-off-by: David Brown <dmlb2000@gmail.com>

--- src/Getit/Getit.jsx
+++ src/Getit/Getit.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Section from '../Section'
+import getText from '../Utils'
 
 const Getit = function Getit () {
   return (
-    <div>
-      {`Get Pacifica Options`}
+    <div className="Getit">
+      {
+        // We are starting with h2 that's why 2
+        // eslint-disable-next-line no-magic-numbers
+        Section(getText().getitpage, 2, 'Getit', null)
+      }
     </div>
   )
 }
--- src/Python/Python.jsx
+++ src/Python/Python.jsx
@@ -1,9 +1,18 @@
+import './index.scss'
 import React from 'react'
+import Section from '../Section'
+import getText from '../Utils'
+
+const imageSrc = require('../assets/PyPi-Logo.svg')
 
 const Python = function Python () {
   return (
-    <div>
-      {`A Display with some PyPi information`}
+    <div className="Python">
+      {
+      // We are starting with h2 that's why 2
+        // eslint-disable-next-line no-magic-numbers
+        Section(getText().pythonpage, 2, 'Python', imageSrc)
+      }
     </div>
   )
 }
