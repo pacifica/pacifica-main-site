566728800ef0748c12def8967bfc47e3edf574e2
David Brown <dmlb2000@gmail.com>
2019-09-04 22:19:06 -0700

    Add contact info page for now
    Signed-off-by: David Brown <dmlb2000@gmail.com>

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -1,9 +1,16 @@
+import './index.scss'
 import React from 'react'
+import Section from '../Section'
+import getText from '../Utils'
 
 const Contact = function Contact () {
   return (
-    <div>
-      {`A Short Contact summary`}
+    <div className="Contact">
+      {
+        // We are starting with h2 that's why 2
+        // eslint-disable-next-line no-magic-numbers
+        Section(getText().contactpage, 2, 'Contact', null)
+      }
     </div>
   )
 }
